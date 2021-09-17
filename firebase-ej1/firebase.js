f99b3044803afd962271672a70bb390feb02607e
Gianissssss <gianicpos@gmail.com>
2021-09-17 16:56:33 -0300

    firebase

---
+++ firebase-ej1/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+        apiKey: "AIzaSyDIgI1wTtEaPDOb16D8B0qoTaGEZH5Gwa0",
+        authDomain: "gianinna-p1.firebaseapp.com",
+        projectId: "gianinna-p1",
+        storageBucket: "gianinna-p1.appspot.com",
+        messagingSenderId: "189002221851",
+        appId: "1:189002221851:web:70d445322cc9e1d55bebbe",
+        measurementId: "G-QL5SD7H755"
+};
+//nos estamos conectando con firebase
+firebase.initializeApp(firebaseConfig)
